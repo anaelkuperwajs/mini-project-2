4b5c925837e1f1c4c81a284a5af7d7df89a855d7
Anael Kuperwajs Cohen <akuperwa@macalester.edu>
2019-11-05 18:24:11 -0600

    All the predictors beside sentiment analysis ones are done!

--- miniProjFunctions.R
+++ miniProjFunctions.R
@@ -1,16 +1,18 @@
 flesch_reading_ease <- function(nwords, nsentences, nsyllables){
-  score = 206.835 - 1.015 * (nwords/nsentences) - 84.6 * (nsyllables/nwords)
+  score = round((206.835 - 1.015 * (nwords/nsentences) - 84.6 * (nsyllables/nwords)), 2)
   return(score)
 }
 
 pct_unique_words <- function(text, nwords){
   unique = length(unique(unlist(str_split(text, ' '))))
-  pct_unique = unique/nwords
+  pct_unique = round((unique/nwords), 2)
   
   return(pct_unique)
 }
 
 exclamation_ratio <- function(text){
   end_point_count = str_count(text, "\\!") + str_count(text, "\\.") + str_count(text, "\\?")
-  return(round((str_count(text, "\\!") / end_point_count)*100, 2))
+  ratio = round((str_count(text, "\\!") / end_point_count)*100, 2)
+  ratio = if_else(ratio == "NaN", 0.00, ratio)
+  return(ratio)
 }
