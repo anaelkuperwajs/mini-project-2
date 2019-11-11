35ede6d7e877f82e8d2ece3003b185c183d8ae65
Anael Kuperwajs Cohen <akuperwa@macalester.edu>
2019-11-11 12:50:26 -0600

    "SENTIMENT"

--- miniProjFunctions.R
+++ miniProjFunctions.R
@@ -16,3 +16,73 @@ exclamation_ratio <- function(text){
   ratio = if_else(ratio == "NaN", 0.00, ratio)
   return(ratio)
 }
+
+max_sentiment_value <- function(text){
+  sentences_vector = get_sentences(text)
+  sentiment = get_nrc_sentiment(sentences_vector)
+  anger = sum(sentiment$anger)
+  anticipation = sum(sentiment$anticipation)
+  disgust = sum(sentiment$disgust)
+  fear = sum(sentiment$fear)
+  joy = sum(sentiment$joy)
+  sadness = sum(sentiment$sadness)
+  surprise = sum(sentiment$surprise)
+  trust = sum(sentiment$trust)
+  negative = sum(sentiment$negative)
+  positive = sum(sentiment$positive)
+  
+  sentiment_vec = c(anger, anticipation, disgust, fear, joy, sadness, surprise, trust, negative, positive)
+  
+  return(max(sentiment_vec))
+}
+
+max_sentiment_type <- function(text){
+  sentences_vector = get_sentences(text)
+  sentiment = get_nrc_sentiment(sentences_vector)
+  anger = sum(sentiment$anger)
+  anticipation = sum(sentiment$anticipation)
+  disgust = sum(sentiment$disgust)
+  fear = sum(sentiment$fear)
+  joy = sum(sentiment$joy)
+  sadness = sum(sentiment$sadness)
+  surprise = sum(sentiment$surprise)
+  trust = sum(sentiment$trust)
+  negative = sum(sentiment$negative)
+  positive = sum(sentiment$positive)
+  
+  sentiment_vec = c(anger, anticipation, disgust, fear, joy, sadness, surprise, trust, negative, positive)
+  index = which.max(sentiment_vec)
+  if(index == 1){
+    return("anger")
+  }
+  else if(index == 2){
+    return("anticipation")
+  }
+  else if(index == 3){
+    return("disgust")
+  }
+  else if(index == 4){
+    return("fear")
+  }
+  else if(index == 5){
+    return("joy")
+  }
+  else if (index == 6){
+    return("sadness")
+  }
+  else if (index == 7){
+    return("surprise")
+  }
+  else if (index == 8){
+    return("trust")
+  }
+  else if(index == 9){
+    return("negative")
+  }
+  else if(index == 10){
+    "positive"
+  }
+  return("??????")
+  
+  
+}
