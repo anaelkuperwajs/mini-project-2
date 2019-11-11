a0ec6e943f65263f878650bcdf5e2a5bcdfb834d
julietkelson <jkelson@macalester.edu>
2019-11-11 17:31:29 -0600

    fixed sentiment analysis

--- miniProjFunctions.R
+++ miniProjFunctions.R
@@ -33,7 +33,7 @@ max_sentiment_value <- function(text){
   
   sentiment_vec = c(anger, anticipation, disgust, fear, joy, sadness, surprise, trust, negative, positive)
   
-  return(max(sentiment_vec))
+  return(max(sentiment_vec)/length(sentences_vector))
 }
 
 max_sentiment_type <- function(text){
@@ -52,37 +52,38 @@ max_sentiment_type <- function(text){
   
   sentiment_vec = c(anger, anticipation, disgust, fear, joy, sadness, surprise, trust, negative, positive)
   index = which.max(sentiment_vec)
-  if(index == 1){
-    return("anger")
-  }
-  else if(index == 2){
-    return("anticipation")
-  }
-  else if(index == 3){
-    return("disgust")
-  }
-  else if(index == 4){
-    return("fear")
-  }
-  else if(index == 5){
-    return("joy")
-  }
-  else if (index == 6){
-    return("sadness")
-  }
-  else if (index == 7){
-    return("surprise")
-  }
-  else if (index == 8){
-    return("trust")
-  }
-  else if(index == 9){
-    return("negative")
-  }
-  else if(index == 10){
-    "positive"
-  }
-  return("??????")
+  return(switch(index, "anger", "anticipation", "disgust", "fear", "joy", "sadness", "surprise", "trust", "negative", "positive"))
+  # if(index == 1){
+  #   return("anger")
+  # }
+  # else if(index == 2){
+  #   return("anticipation")
+  # }
+  # else if(index == 3){
+  #   return("disgust")
+  # }
+  # else if(index == 4){
+  #   return("fear")
+  # }
+  # else if(index == 5){
+  #   return("joy")
+  # }
+  # else if (index == 6){
+  #   return("sadness")
+  # }
+  # else if (index == 7){
+  #   return("surprise")
+  # }
+  # else if (index == 8){
+  #   return("trust")
+  # }
+  # else if(index == 9){
+  #   return("negative")
+  # }
+  # else if(index == 10){
+  #   return("positive")
+  # }
+  # return("??????")
   
   
 }
